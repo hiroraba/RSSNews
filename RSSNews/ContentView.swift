b5cecab1bc44448a9ab97fea3e252f4cccdc5137
hiroraba <xornelius0313@gmail.com>
2026-04-02 23:05:32 +0900

    ContentView に Preview を追加する

--- RSSNews/ContentView.swift
+++ RSSNews/ContentView.swift
@@ -4,9 +4,15 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
     var body: some View {
         RootTabView()
     }
+}
+
+#Preview {
+    ContentView()
+        .modelContainer(for: [RSSFeed.self, Article.self], inMemory: true)
 }
